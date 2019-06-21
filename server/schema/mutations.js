d90d38077e52dc22e50a2fbd3319477982fdb819
Conley Potter <conley.potter@gmail.com>
2019-06-21 16:22:22 -0500

    Finish backend for games and console -pre-testing

--- server/schema/mutations.js
+++ server/schema/mutations.js
@@ -1,9 +1,13 @@
 const graphql = require('graphql');
 const mongoose = require('mongoose');
-const UserType = require('./user_type');
+const UserType = require('./types/user_type');
 const User = mongoose.model('user');
+const GameType = require('./types/game_type');
+const Game = mongoose.model('game');
+const ConsoleType = require('./types/console_type');
+const Console = mongoose.model('console');
 
-const { GraphQLObjectType, GraphQLString, GraphQLNonNull } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLNonNull, GraphQLID } = graphql;
 
 const Mutation = new GraphQLObjectType({
   name: "Mutation",
@@ -22,8 +26,45 @@ const Mutation = new GraphQLObjectType({
           password
         }).save();
       }
+    },
+    newGame: {
+      type: GameType,
+      args: {
+        name: { type: GraphQLString },
+        // user: { type: GraphQLID },
+        description: { type: GraphQLString },
+        releaseDate: { type: GraphQLString },
+        console: { type: GraphQLID }
+      },
+      resolve(_, { name, description, releaseDate, console }) {
+        return new Game({ name, description, releaseDate, console }).save();
+      }
+    },
+    deleteGame: {
+      type: GameType,
+      args: { id: { type: GraphQLID } },
+      resolve(_, { id }) {
+        return Game.deleteOne({ _id: id });
+      }
+    },
+    newConsole: {
+      type: ConsoleType,
+      args: {
+        name: { type: GraphQLString },
+      },
+      resolve(_, { name }) {
+        return new Console({ name }).save();
+      }
+    },
+    deleteConsole: {
+      type: ConsoleType,
+      args: { id: { type: GraphQLID } },
+      resolve(_, { id }) {
+        return Console.deleteOne({ _id: id });
+      }
     }
   }
 });
 
 module.exports = Mutation;
+
