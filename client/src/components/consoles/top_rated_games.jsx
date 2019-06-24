28dfa801a6e89aa34d18ddd54289f28e5427e0c1
John-Michael Riley <johnmichael.w.riley@gmail.com>
2019-06-24 15:31:08 -0400

    add basic styling to top games list on console page

--- client/src/components/consoles/top_rated_games.jsx
+++ client/src/components/consoles/top_rated_games.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 
 const TopRatedGames = ({ games }) => {
   return (
@@ -7,17 +8,19 @@ const TopRatedGames = ({ games }) => {
       <ul className='console-top-games-list'>
         {games.map((game, i) => {
           return (
-            <div className='console-top-games-item'>
-              <div>{i + 1}</div>
-              <div className='top-games-thumbnail'>
-                <img src={game.imageURL} alt={`${game.name} image`} />
-              </div>
-              <div className="top-games-game-info">
-              <div>{game.name}</div>
+            <Link to={`/games/${game._id}`}>
+              <div className='console-top-games-item'>
+                <div>{i + 1}</div>
+                <div className='top-games-thumbnail'>
+                  <img src={game.imageURL} alt={`${game.name} image`} />
+                </div>
+                <div className="top-games-game-info">
+                <div>{game.name}</div>
 
-              <div className='game-rating'>Rating: <span>{`${game.rating}%`}</span></div>
+                <div className='game-rating'>Rating: <span>{`${game.rating}%`}</span></div>
+                </div>
               </div>
-            </div>
+            </Link>
           );
         })}
       </ul>
