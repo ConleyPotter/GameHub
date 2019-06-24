c7131e572180fd62e2475a90cf5e922887797021
John-Michael Riley <johnmichael.w.riley@gmail.com>
2019-06-24 13:52:49 -0400

    add likes and dislikes to game schema | method to return overall rating on game | query for top 10 games by console

--- server/schema/types/game_type.js
+++ server/schema/types/game_type.js
@@ -1,27 +1,36 @@
 const mongoose = require('mongoose');
 const graphql = require('graphql');
-const { GraphQLObjectType, GraphQLString, GraphQLID } = graphql;
-
+const { GraphQLObjectType, GraphQLString, GraphQLID, GraphQLInt } = graphql;
+const GameSchema = require('../../models/Game');
+const Game = mongoose.model('game');
 const Console = mongoose.model('console');
 
 const GameType = new GraphQLObjectType({
-	name: 'GameType',
-	fields: () => ({
-		_id: { type: GraphQLID },
-		name: { type: GraphQLString },
-		description: { type: GraphQLString },
-		releaseDate: { type: GraphQLString },
-		imageURL: { type: GraphQLString },
-		videoUrl: { type: GraphQLString },
-		console: {
-			type: require('./console_type'),
-			resolve(parentValue) {
-				return Console.findById(parentValue.console)
-					.then(console => console)
-					.catch(err => console.log(err));
-			}
-		}
-	})
+  name: 'GameType',
+  fields: () => ({
+    _id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+    releaseDate: { type: GraphQLString },
+    imageURL: { type: GraphQLString },
+    videoUrl: { type: GraphQLString },
+    console: {
+      type: require('./console_type'),
+      resolve(parentValue) {
+        return Console.findById(parentValue.console)
+          .then(console => console)
+          .catch(err => console.log(err));
+      }
+    },
+    likes: { type: GraphQLInt },
+    dislikes: { type: GraphQLInt },
+    rating: {
+      type: GraphQLInt,
+      resolve(parentValue) {
+        return parentValue.rating();
+      }
+    }
+  })
 });
 
 module.exports = GameType;
--- server/schema/types/root_query_type.js
+++ server/schema/types/root_query_type.js
@@ -7,65 +7,78 @@ const ConsoleType = require('./console_type');
 const Game = mongoose.model('game');
 const GameType = require('./game_type');
 
-const { GraphQLObjectType, GraphQLList, GraphQLNonNull, GraphQLID, GraphQLString } = graphql;
+const {
+  GraphQLObjectType,
+  GraphQLList,
+  GraphQLNonNull,
+  GraphQLID,
+  GraphQLString
+} = graphql;
 
 const RootQuery = new GraphQLObjectType({
-	name: 'RootQueryType',
-	fields: {
-		users: {
-			type: new GraphQLList(UserType),
-			resolve() {
-				return User.find({});
-			}
-		},
-		user: {
-			type: UserType,
-			args: { id: { type: new GraphQLNonNull(GraphQLID) } },
-			resolve(parentValue, { id }) {
-				return User.findById(id);
-			}
-		},
-		consoles: {
-			type: new GraphQLList(ConsoleType),
-			resolve() {
-				return Console.find({});
-			}
-		},
-		console: {
-			type: ConsoleType,
-			args: { id: { type: new GraphQLNonNull(GraphQLID) } },
-			resolve(_, { id }) {
-				return Console.findById(id);
-			}
-		},
-		consoleByURL: {
-			type: ConsoleType,
-			args: { url: { type: GraphQLString } },
-			resolve(_, { url }) {
-				return Console.findOne({ url: url });
-			}
-		},
-		games: {
-			type: new GraphQLList(GameType),
-			args: {
-				name: { type: GraphQLString },
-				description: { type: GraphQLString },
-				releasedAfter: { type: GraphQLString },
-				releasedBefore: { type: GraphQLString },
-				consoleName: { type: GraphQLString }
-			},
-			resolve: async (_, filters) => {
-				return await Game.findByFilters(filters);
-			}
-		},
-		game: {
-			type: GameType,
-			args: { id: { type: new GraphQLNonNull(GraphQLID) } },
-			resolve(_, { id }) {
-				return Game.findById(id);
-			}
-		}
-	}
+  name: 'RootQueryType',
+  fields: {
+    users: {
+      type: new GraphQLList(UserType),
+      resolve() {
+        return User.find({});
+      }
+    },
+    user: {
+      type: UserType,
+      args: { id: { type: new GraphQLNonNull(GraphQLID) } },
+      resolve(parentValue, { id }) {
+        return User.findById(id);
+      }
+    },
+    consoles: {
+      type: new GraphQLList(ConsoleType),
+      resolve() {
+        return Console.find({});
+      }
+    },
+    console: {
+      type: ConsoleType,
+      args: { id: { type: new GraphQLNonNull(GraphQLID) } },
+      resolve(_, { id }) {
+        return Console.findById(id);
+      }
+    },
+    consoleByURL: {
+      type: ConsoleType,
+      args: { url: { type: GraphQLString } },
+      resolve(_, { url }) {
+        return Console.findOne({ url: url });
+      }
+    },
+    games: {
+      type: new GraphQLList(GameType),
+      args: {
+        name: { type: GraphQLString },
+        description: { type: GraphQLString },
+        releasedAfter: { type: GraphQLString },
+        releasedBefore: { type: GraphQLString },
+        consoleName: { type: GraphQLString }
+      },
+      resolve: async (_, filters) => {
+        return await Game.findByFilters(filters);
+      }
+    },
+    game: {
+      type: GameType,
+      args: { id: { type: new GraphQLNonNull(GraphQLID) } },
+      resolve(_, { id }) {
+        return Game.findById(id);
+      }
+    },
+    topGamesByConsole: {
+      type: new GraphQLList(GameType),
+      args: { id: { type: GraphQLID } },
+      resolve(_, { id }) {
+        return Console.findTopGames(id);
+      }
+    }
+  }
 });
 
 module.exports = RootQuery;
