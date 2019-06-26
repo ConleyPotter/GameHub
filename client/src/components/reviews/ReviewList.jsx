f489f748448d514314fe7371b3c26dde245dc52f
Bryce Morgan <bamorgan13@gmail.com>
2019-06-26 18:57:59 -0400

    Restructure liked flag on review list items. Add styling

--- client/src/components/reviews/ReviewList.jsx
+++ client/src/components/reviews/ReviewList.jsx
@@ -4,24 +4,28 @@ import './review_list.scss';
 
 export default function ReviewIndex({ reviews }) {
 	const reviewsWithContent = reviews.filter(review => review.content);
-	const reviewLis = reviewsWithContent.map(review => (
-		<li className={`${review.liked ? 'liked ' : 'disliked '}review-list-item`} key={review._id}>
-			<div className="user-info">
-				<img className="avatar" src={`${review.user.avatar}`} alt={`${review.user.username} avatar`} />
-				<h5 className="username">{review.user.username}</h5>
-			</div>
-			<div className="review-details">
-				<h3 className="title">{review.title}</h3>
-				{review.liked ? (
-					<p className="liked-label">This user liked this game.</p>
-				) : (
-					<p className="disliked-label">This user disliked this game.</p>
-				)}
-				<p className="content">{review.content}</p>
-				<p className="publish-date">Review published: {formatDate(review.date)}</p>
-			</div>
-		</li>
-	));
+	const reviewLis = reviewsWithContent.map(review => {
+		const likedFlag = review.liked ? 'liked ' : 'disliked ';
+		return (
+			<li className={`${likedFlag}review-list-item`} key={review._id}>
+				<div className="user-info">
+					<img
+						className="avatar"
+						src={`${review.user.avatar ||
+							'https://ryanacademy.ie/wp-content/uploads/2017/04/user-placeholder.png'}`}
+						alt={`${review.user.username} avatar`}
+					/>
+					<h5 className="username">{review.user.username}</h5>
+				</div>
+				<div className="review-details">
+					<h3 className="title">{review.title}</h3>
+					<p className="like-label">This user {likedFlag}this game.</p>
+					<p className="content">{review.content}</p>
+					<p className="publish-date">Review published: {formatDate(review.date)}</p>
+				</div>
+			</li>
+		);
+	});
 	return reviewLis.length > 0 ? (
 		<div className="published-reviews">
 			<h3 className="detail-field-label">User Reviews</h3>
