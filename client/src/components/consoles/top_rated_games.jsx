2060a8295a7c76641ca6ad02030747234845707b
John-Michael Riley <johnmichael.w.riley@gmail.com>
2019-06-26 17:03:05 -0400

    add top games list selector for all consoles to splash page

--- client/src/components/consoles/top_rated_games.jsx
+++ client/src/components/consoles/top_rated_games.jsx
@@ -1,30 +1,29 @@
 import React from 'react';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 const TopRatedGames = ({ games }) => {
   return (
-    <div className='console-top-games-container'>
-      <div className='console-top-games-header'>Top Rated Games</div>
-      <ul className='console-top-games-list'>
-        {games.map((game, i) => {
-          return (
-            <Link to={`/games/${game._id}`}>
-              <div className='console-top-games-item'>
-                <div>{i + 1}</div>
-                <div className='top-games-thumbnail'>
-                  <img src={game.imageURL} alt={`${game.name} image`} />
-                </div>
-                <div className="top-games-game-info">
+    <ul className='console-top-games-list'>
+      {games.map((game, i) => {
+        return (
+          <Link to={`/games/${game._id}`}>
+            <div className='console-top-games-item'>
+              <div>{i + 1}</div>
+              <div className='top-games-thumbnail'>
+                <img src={game.imageURL} alt={`${game.name} image`} />
+              </div>
+              <div className='top-games-game-info'>
                 <div>{game.name}</div>
 
-                <div className='game-rating'>Rating: <span>{`${game.rating}%`}</span></div>
+                <div className='game-rating'>
+                  Rating: <span>{`${game.rating}%`}</span>
                 </div>
               </div>
-            </Link>
-          );
-        })}
-      </ul>
-    </div>
+            </div>
+          </Link>
+        );
+      })}
+    </ul>
   );
 };
 
