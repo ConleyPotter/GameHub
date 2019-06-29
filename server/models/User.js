0713ab1a5c269ea681d5fe60b81b73b73249114c
Bryce Morgan <bamorgan13@gmail.com>
2019-06-28 21:44:26 -0400

    Add lists to user schema

--- server/models/User.js
+++ server/models/User.js
@@ -2,74 +2,92 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true,
-    min: 8,
-    max: 32
-  },
-  reviews: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: 'review'
-    }
-  ],
-  surveys: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: 'survey'
-    }
-  ],
-  admin: {
-    type: Boolean,
-    default: false
-  }
+	username: {
+		type: String,
+		required: true,
+		unique: true
+	},
+	email: {
+		type: String,
+		required: true,
+		unique: true
+	},
+	password: {
+		type: String,
+		required: true,
+		min: 8,
+		max: 32
+	},
+	reviews: [
+		{
+			type: Schema.Types.ObjectId,
+			ref: 'review'
+		}
+	],
+	surveys: [
+		{
+			type: Schema.Types.ObjectId,
+			ref: 'survey'
+		}
+	],
+	lists: [
+		{
+			type: Schema.Types.ObjectId,
+			ref: 'list'
+		}
+	],
+	admin: {
+		type: Boolean,
+		default: false
+	}
 });
 
 UserSchema.statics.findReviews = function(userId) {
-  return this.findById(userId)
-    .populate('reviews')
-    .then(user => user.reviews);
+	return this.findById(userId)
+		.populate('reviews')
+		.then(user => user.reviews);
 };
 
 UserSchema.statics.addReview = function({ userId, reviewId }) {
-  const User = mongoose.model('user');
-  const Review = mongoose.model('review');
+	const User = mongoose.model('user');
+	const Review = mongoose.model('review');
 
-  return User.findById(userId).then(user => {
-    return Review.findById(reviewId).then(async review => {
-      user.reviews.push(review);
-      return await user.save();
-    });
-  });
+	return User.findById(userId).then(user => {
+		return Review.findById(reviewId).then(async review => {
+			user.reviews.push(review);
+			return await user.save();
+		});
+	});
+};
+
+UserSchema.statics.addList = function({}) {
+	const User = mongoose.model('user');
+	const List = mongoose.model('list');
+
+	return User.findById(userId).then(async user => {
+		return List.findById(listId).then(async list => {
+			user.lists.push(list);
+			return await user.save();
+		});
+	});
 };
 
 UserSchema.statics.findSurveys = function(userId) {
-  return this.findById(userId)
-    .populate('surveys')
-    .then(user => user.surveys);
+	return this.findById(userId)
+		.populate('surveys')
+		.then(user => user.surveys);
 };
 
 UserSchema.statics.addSurvey = function({ userId, surveyId }) {
-  const User = mongoose.model('user');
-  const Survey = mongoose.model('survey');
+	const User = mongoose.model('user');
+	const Survey = mongoose.model('survey');
 
-  return User.findById(userId).then(user => {
-    return Survey.findById(surveyId).then(async survey => {
-      user.surveys.push(survey);
-      return await user.save();
-    });
-  });
+	return User.findById(userId).then(user => {
+		return Survey.findById(surveyId).then(async survey => {
+			user.surveys.push(survey);
+			return await user.save();
+		});
+	});
 };
 
 module.exports = mongoose.model('user', UserSchema);
