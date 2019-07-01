f9e15057bdeb6a05b6e35d43b8737111a7ea5346
Bryce Morgan <bamorgan13@gmail.com>
2019-07-01 14:29:20 -0400

    Add path to app.js and heroku-postbuild script

--- app.js
+++ app.js
@@ -5,9 +5,17 @@ const cors = require('cors');
 const expressGraphQL = require('express-graphql');
 const models = require('./server/models/index');
 const schema = require('./server/schema/schema');
+const path = require('path');
 
 const app = express();
 const db = require('./config/keys').mongoURI;
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+	app.get('/', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
 
 mongoose
 	.connect(db, {
