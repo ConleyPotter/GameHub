c44e4cb81fb3d536c73a734bc93e14ea1f74465b
Bryce Morgan <bamorgan13@gmail.com>
2019-07-12 17:36:44 -0400

    Add process.env.PORT to httplink

--- client/src/index.js
+++ client/src/index.js
@@ -30,7 +30,7 @@ cache.writeData({
 });
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:5000/graphql'
+  uri: `http://localhost:${process.env.PORT || 5000}/graphql`
 });
 
 const authLink = setContext((_, { headers }) => {
